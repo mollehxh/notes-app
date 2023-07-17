fff8ae50a01894ad4c8d108c0f0286cfce540f76
mollehxh <mollehxh@gmail.com>
2023-07-16 19:57:34 -0700

    chore: add page-loader

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,10 @@
+export { AppShell } from './app-shell';
+export { ButtonBase } from './button-base';
+export { Button } from './button';
+export { IconButton } from './icon-button';
+export { Header } from './header';
+export { PageLoader } from './page-loader';
+export { List } from './list';
+export { ListItem } from './list-item';
+export { TextField } from './text-field';
+export { Sidebar } from './sidebar';
---
+++ src/shared/ui/page-loader/page-loader.tsx
@@ -0,0 +1,19 @@
+import './page-loader.scss';
+
+export const PageLoader = () => {
+  return (
+    <div className="page-loader">
+      <div className="lds-grid">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
